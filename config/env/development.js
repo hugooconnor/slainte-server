765812e7c1dfb26f12899bf274c9bb9415473441
Hugo O'Connor <hugo.oconnor@gmail.com>
2017-02-07 16:23:15 +1100

    init changes

--- config/env/development.js
+++ config/env/development.js
@@ -2,7 +2,7 @@ const devConfig = {
   env: 'development',
   MONGOOSE_DEBUG: true,
   jwtSecret: '0a6b944d-d2fb-46fc-a85e-0295c986cd9f',
-  db: 'mongodb://localhost/express-mongoose-es6-rest-api-development',
+  db: '',
   port: 4040
 };
 
--- config/env/index.js
+++ config/env/index.js
@@ -2,6 +2,8 @@ import path from 'path';
 
 const env = process.env.NODE_ENV || 'development';
 const config = require(`./${env}`); // eslint-disable-line import/no-dynamic-require
+const dotenv = require('dotenv').config(); // eslint-disable-line 
+config.db = dotenv.parsed.MONGOURL;
 
 const defaults = {
   root: path.join(__dirname, '/..')
